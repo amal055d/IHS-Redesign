9b8ec94032fcf9a5233217f144cd459c37794c0f
felixb97 <fillefish@hotmail.com>
2020-11-04 23:34:50 +0100

    api preparation

---
+++ javascript.js
@@ -0,0 +1,63 @@
+let theData;
+
+
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+
+    hentData();
+}
+
+async function hentData() {
+    let url = "http://signemariemathiasen.dk/kea/2_sem/09.05.01_ihs/wordpress/wp-json/wp/v2/etFag?per_page=100";
+
+    let jsonData = await fetch(url);
+    etFag = await jsonData.json();
+    console.log("hentData(json)");
+    loadGlobalContent();
+    vis(etFag);
+
+
+}
+
+function vis(data) {
+    console.log(data);
+}
+
+function loadGlobalContent() {
+    loadHeaderContent();
+    loadFooterContent();
+    console.log("loadGlobalContent");
+}
+
+
+function loadHeaderContent() {
+    console.log("loadHeader");
+   document.querySelector("#logo img").src = etFag.media.guid;
+}
+
+function loadFooterContent() {
+    console.log("loadFooter");
+    //document.querySelector()
+}
+
+function loadFrontContent() {
+    console.log("loadForside");
+}
+
+function loadOmContent() {
+    console.log("loadOmOs");
+}
+
+function loadFagContent() {
+    console.log("loadFag");
+}
+
+function loadPratiskContent() {
+    console.log("loadPratisk");
+}
+
+function loadTilmeldingContent() {
+    console.log("loadTilmeling");
+}
